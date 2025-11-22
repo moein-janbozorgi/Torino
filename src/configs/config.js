56c312273a37b3ce23b6158f3bcd6cd1aac0f93c
moein-janbozorgi <moeinjanb84@gmail.com>
2025-11-22 15:20:33 +0330

    some changes for deployed

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,7 +2,7 @@ import { getCookie, setCookie } from "@/utils/cookieHelper";
 
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
